9468249aae8a672cd4b2ae74127187092d1d3af0
LDRushing <rushinglucy@yahoo.com>
2026-05-05 15:09:29 -0500

    your spam folder

--- VCfrontend/vixencomixapp-frontend/src/Contact.jsx
+++ VCfrontend/vixencomixapp-frontend/src/Contact.jsx
@@ -94,7 +94,7 @@ export default function Contact() {
         >
           Send Message
         </button>
-        <h3>Please check Spam folder for responses.</h3>
+        <h3>Please check your Spam folder for responses.</h3>
       </form>
     </div>
   );
